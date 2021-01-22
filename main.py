47d65426987d1de67d0ad5dd8d63b8817a95c79e
Raveen Panditha <raveenpanditha9@gmail.com>
2021-01-22 09:58:58 +0000

    datetime code

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 #datetime
+import datetime
 
+today = datetime.date.today()
+print(today)
+
+birthday = datetime.date(1996, 5, 2)
+print(birthday)
+
+days_since_bday = (today-birthday).days
+
+print(days_since_bday)
