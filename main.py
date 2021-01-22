0dec2ded54ad2414a24222705d13901bd90d7e03
Raveen Panditha <raveenpanditha9@gmail.com>
2021-01-22 20:58:01 +0000

    Webscraping

--- main.py
+++ main.py
@@ -1,28 +1,7 @@
-#datetime
-import datetime
-import pytz
+#webscraping
+import requests
+from bs4 import BeautifulSoup
 
-
-today = datetime.date.today()
-print(today)
-
-birthday = datetime.date(1996, 5, 2)
-print(birthday)
-
-days_since_bday = (today-birthday).days
-
-print(days_since_bday)
-
-tdelta = datetime.timedelta(days = 10)
-print(today + tdelta)
-print(today - tdelta)
-
-
-print(today.day)
-print(today.weekday())
-
-print(datetime.time(7,2,20,15))
-
-hour_delta = datetime.timedelta(hours=10)
-print(datetime.datetime.now() + hour_delta)
-print(datetime.datetime.now(tz=pytz.UTC))
+page = requests.get('https://www.weather.gov/')
+soup = BeautifulSoup(page.content, 'html.parser')
+print(soup.findall('a'))
