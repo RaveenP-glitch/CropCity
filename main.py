a18048a10b44aed8cf0b38d101b6f4847f2e98b6
Raveen Panditha <raveenpanditha9@gmail.com>
2021-01-22 11:38:12 +0000

    using pytz

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #datetime
 import datetime
+import pytz
+
 
 today = datetime.date.today()
 print(today)
@@ -10,3 +12,17 @@ print(birthday)
 days_since_bday = (today-birthday).days
 
 print(days_since_bday)
+
+tdelta = datetime.timedelta(days = 10)
+print(today + tdelta)
+print(today - tdelta)
+
+
+print(today.day)
+print(today.weekday())
+
+print(datetime.time(7,2,20,15))
+
+hour_delta = datetime.timedelta(hours=10)
+print(datetime.datetime.now() + hour_delta)
+print(datetime.datetime.now(tz=pytz.UTC))
