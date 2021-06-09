dbcc73e10353728aea87d585c2aa3b82872cfc21
Raveen Panditha <raveenpanditha8@gmail.com>
2021-06-09 19:27:32 +0530

    changes

--- main.py
+++
@@ -1,9 +0,0 @@
-#webscraping
-import requests
-from bs4 import BeautifulSoup
-
-page = requests.get('https://www.weather.gov/')
-soup = BeautifulSoup(page.content, 'html.parser')
-print(soup.findall('a'))
-print("Im a new boss")
-print("second line")
--- part_manager.py
+++ part_manager.py
@@ -7,28 +7,28 @@ app = Tk()
 part_text = StringVar()
 part_label = Label(app, text='Part Name', font=('bold', 14), pady=20, padx=20)
 part_label.grid(row=0, column=0, sticky=W)
-part_entry = Entry(app, textvariable=part_text, width=46)
+part_entry = Entry(app, textvariable=part_text, width=40)
 part_entry.grid(row=0, column=1)
 
 #Customer
 customer_text = StringVar()
 customer_label = Label(app, text='Customer', font=('bold', 14), padx=20)
 customer_label.grid(row=1, column=2, sticky=W)
-customer_entry = Entry(app, textvariable=part_text, width=46)
+customer_entry = Entry(app, textvariable=part_text, width=40)
 customer_entry.grid(row=1, column=3)
 
 #Retailer
 retailer_text = StringVar()
 retailer_label = Label(app, text='Retailer', font=('bold', 14), pady=20, padx=20)
 retailer_label.grid(row=1, column=0, sticky=W)
-retailer_entry = Entry(app, textvariable=part_text, width=46)
+retailer_entry = Entry(app, textvariable=part_text, width=40)
 retailer_entry.grid(row=1, column=1)
 
 #Price
 part_text = StringVar()
 part_label = Label(app, text='Price', font=('bold', 14), pady=20, padx=20)
 part_label.grid(row=0, column=2, sticky=W)
-part_entry = Entry(app, textvariable=part_text, width=46)
+part_entry = Entry(app, textvariable=part_text, width=40)
 part_entry.grid(row=0, column=3)
 
 #Parts List (Listbox)
